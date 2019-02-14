26141a9c6b9421dbeff438cea3f73fd1c74ea1fa
Sarvar <sarvar9818sh@gmail.com>
2019-02-14 21:42:19 +0300

    firt load

---
+++ index_2.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>massiv</title>
+</head>
+<body>
+
+<?php 
+$massiv=array(10,'ismi','ish joyi',true);
+	if($massiv[0]<=25 && $massiv[3]==true){
+		echo '<center><h1>yaroqli</h1></center>';
+	}else {
+		echo '<center><h1>yaroqsiz</h1></center>';
+	}
+?>
+</body>
+</html>
+
+
